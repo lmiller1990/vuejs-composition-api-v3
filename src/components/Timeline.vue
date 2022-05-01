e886c2b440672ad86b372a1e9f57706243745a61
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-01 22:27:38 +1000

    7 - events

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 const periods = ["Today", "This Week", "This Month"]
+
+let selectedPeriod = "Today"
+
+function selectPeriod (period: string) {
+  console.log(period)
+  selectedPeriod = period
+}
 </script>
 
 <template>
   <nav class="is-primary panel">
+    {{ selectedPeriod }}
     <span class="panel-tabs">
       <a 
         v-for="period of periods"
         :key="period"
+        @click="selectPeriod(period)"
       >
         {{ period }}
       </a>
