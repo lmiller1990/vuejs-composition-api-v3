77495280f43892e2cead11872f18e3ae5280c2c2
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-01 20:20:05 +1000

    lecture 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Timeline from './components/Timeline.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="section">
+    <div class="container">
+      <Timeline />
+    </div>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
 </style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a>Today</a>
+      <a>This Week</a>
+      <a>This Month</a>
+    </span>
+  </nav>
+</template>
