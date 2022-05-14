875e910b19384045825a1ae4a412d4ed0e7c4f94
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-14 19:43:12 +1000

    27 - marked w/ watchEffect

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch, watchEffect } from "vue";
 import { TimelinePost } from "../posts";
+import { marked } from "marked"
 
 const props = defineProps<{
   post: TimelinePost;
@@ -8,7 +9,16 @@ const props = defineProps<{
 
 const title = ref(props.post.title);
 const content = ref(props.post.markdown);
+const html = ref('')
 const contentEditable = ref<HTMLDivElement>();
+
+watch(content, (newContent) => {
+  marked.parse(newContent, (err, parseResult) => {
+    html.value = parseResult
+  })
+}, {
+  immediate: true
+})
 
 onMounted(() => {
   if (!contentEditable.value) {
@@ -46,7 +56,7 @@ function handleInput() {
         @input="handleInput" />
     </div>
     <div class="column">
-      {{ content }}
+      <div v-html="html" />
     </div>
   </div>
 </template>
