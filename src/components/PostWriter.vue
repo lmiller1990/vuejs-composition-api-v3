358bf05797d9d1cffcb2e362a4a986711edf2232
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-14 17:26:11 +1000

    23 - post write

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { TimelinePost } from '../posts';
+
+const props = defineProps<{
+  post: TimelinePost
+}>()
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">Post title</div>
+        <input type="text" class="input">
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,3 +1,16 @@
+<script lang="ts" setup>
+import { DateTime } from "luxon"
+import PostWriter from '../components/PostWriter.vue';
+import { TimelinePost } from '../posts';
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "Title",
+  created: DateTime.now()
+}
+</script>
+
 <template>
   New Post
+  <PostWriter :post="post" />
 </template>
