2ed1065138c682d4fef8f928cb33bd6afd716a3b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-05-14 22:06:23 +1000

    server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,22 @@
 import express from 'express'
 import cors from 'cors'
-import { today, thisWeek, thisMonth } from "../posts"
+import bodyParser from 'body-parser'
+import { today, thisWeek, thisMonth, Post } from "../posts"
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+
+const allPosts = [today, thisWeek, thisMonth]
 
 app.get("/posts", (req, res) => {
-  res.json([today, thisWeek, thisMonth])
+  res.json(allPosts)
+})
+
+app.post<{}, {}, Post>("/posts", (req, res) => {
+  const post = {...req.body, id: (Math.random() * 100000).toFixed() }
+  allPosts.push(post)
+  res.json(post)
 })
 
 app.listen(8000, () => {
