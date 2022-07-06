d541d2802dd5e19d8d42620cd92e9e0a90a45cc6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-06 17:01:17 +1000

    50 - generic user form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,58 +1,18 @@
 <script lang="ts" setup>
-import { computed, ref, useSlots } from 'vue';
-import FormInput from './FormInput.vue';
-import { NewUser } from '../users'
-import { validate, length, required } from '../validation'
-import { useUsers } from '../stores/users'
-import { useModal } from '../composables/modal';
-
-const username = ref('')
-const usernameStatus = computed(() => {
-  return validate(username.value, [required, length({ min: 5, max: 10 })])
-})
-
-const password = ref('')
-const passwordStatus = computed(() => {
-  return validate(password.value, [required, length({ min: 10, max: 40 })])
-})
-
-const isInvalid = computed(() => {
-  return (!usernameStatus.value.valid || !passwordStatus.value.valid)
-})
+import { useModal } from "../composables/modal";
+import { useUsers } from "../stores/users";
+import { NewUser } from "../users";
+import UserForm from "./UserForm.vue"
 
 const usersStore = useUsers()
 const modal = useModal()
 
-async function handleSubmit () {
-  if (isInvalid.value) {
-    return
-  }
-
-  const newUser: NewUser = {
-    username: username.value,
-    password: password.value,
-  }
-
-  try {
-    await usersStore.createUser(newUser)
-  } catch (e) {}
-
-  modal.hideModal()
+async function handleSignup (newUser: NewUser) {
+  await usersStore.createUser(newUser)
+  modal.hideModal();
 }
 </script>
 
 <template>
-  <form class="form" @submit.prevent="handleSubmit">
-    <FormInput name="Username" v-model="username" :status="usernameStatus" type="text" />
-    <FormInput name="Password" v-model="password" :status="passwordStatus" type="password" />
-    <button class="button" :disabled="isInvalid">Submit</button>
-  </form>
+  <UserForm @submit="handleSignup" />
 </template>
-
-<style scoped>
-.form {
-  background: white;
-  padding: 30px;
-  margin-top: 50px;
-}
-</style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import { computed, ref, useSlots } from 'vue';
+import FormInput from './FormInput.vue';
+import { NewUser } from '../users'
+import { validate, length, required } from '../validation'
+import { useUsers } from '../stores/users'
+import { useModal } from '../composables/modal';
+
+const emit = defineEmits<{
+  (event: 'submit', payload: NewUser): void
+}>()
+
+const username = ref('')
+const usernameStatus = computed(() => {
+  return validate(username.value, [required, length({ min: 5, max: 10 })])
+})
+
+const password = ref('')
+const passwordStatus = computed(() => {
+  return validate(password.value, [required, length({ min: 10, max: 40 })])
+})
+
+const isInvalid = computed(() => {
+  return (!usernameStatus.value.valid || !passwordStatus.value.valid)
+})
+
+const usersStore = useUsers()
+const modal = useModal()
+
+async function handleSubmit () {
+  if (isInvalid.value) {
+    return
+  }
+
+  const newUser: NewUser = {
+    username: username.value,
+    password: password.value,
+  }
+
+  try {
+    emit('submit', newUser)
+  } catch (e) {}
+}
+</script>
+
+<template>
+  <form class="form" @submit.prevent="handleSubmit">
+    <FormInput name="Username" v-model="username" :status="usernameStatus" type="text" />
+    <FormInput name="Password" v-model="password" :status="passwordStatus" type="password" />
+    <button class="button" :disabled="isInvalid">Submit</button>
+  </form>
+</template>
+
+<style scoped>
+.form {
+  background: white;
+  padding: 30px;
+  margin-top: 50px;
+}
+</style>
