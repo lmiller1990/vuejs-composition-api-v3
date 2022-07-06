ad7fb29f6cbfa45ca6f09c4b94bcf783909eecc1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-06 11:24:43 +1000

    45 - jwt signing

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import jsonwebtoken from 'jsonwebtoken'
 import { today, thisWeek, thisMonth, Post } from "../posts"
 import { NewUser, User } from '../users'
 
 const app = express()
 app.use(cors())
+app.use(cookieParser())
 app.use(bodyParser.json())
 
 const allPosts = [today, thisWeek, thisMonth]
@@ -21,9 +24,21 @@ app.post<{}, {}, Post>("/posts", (req, res) => {
   res.json(post)
 })
 
+const SECRET = 'my-secret'
+const COOKIE = 'vuejs-jwt'
+
+function authenticate (id: string, req: Request, res: Response) {
+  const token = jsonwebtoken.sign({ id }, SECRET, {
+    issuer: 'vuejs-course',
+    expiresIn: '30 days'
+  })
+  res.cookie(COOKIE, token, { httpOnly: true })
+}
+
 app.post<{}, {}, NewUser>("/users", (req, res) => {
   const user: User = {...req.body, id: (Math.random() * 100000).toFixed() }
   allUsers.push(user)
+  authenticate(user.id, req, res)
   const { password, ...rest } = user
   res.json(rest)
 })
