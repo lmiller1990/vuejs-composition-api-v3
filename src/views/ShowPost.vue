2c41a7d7cc2fca2b65059732981f4a3876125917
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-07 23:09:33 +1000

    add check

--- src/views/ShowPost.vue
+++ src/views/ShowPost.vue
@@ -1,25 +1,43 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router';
-import { usePosts } from '../stores/posts';
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import { usePosts } from "../stores/posts";
+import { useUsers } from "../stores/users";
 
 const route = useRoute();
 const postsStore = usePosts();
+const usersStore = useUsers();
 
-const id = route.params.id as string
-const post = postsStore.all.get(id)
+const id = route.params.id as string;
+const post = postsStore.all.get(id);
 
 if (!post) {
-  throw Error(`Post with id ${id} was not found`)
+  throw Error(`Post with id ${id} was not found`);
 }
+
+const canEdit = computed(() => {
+  if (!usersStore.currentUserId) {
+    return false;
+  }
+
+  if (usersStore.currentUserId !== post.authorId) {
+    return false;
+  }
+
+  return true;
+});
 </script>
 
 <template>
   <div class="columns">
     <div class="column"></div>
     <div class="column is-two-thirds">
-      <RouterLink 
+      <RouterLink
+        v-if="canEdit"
         :to="`/posts/${post.id}/edit`"
-        class="is-link button is-rounded">Edit Post</RouterLink>
+        class="is-link button is-rounded"
+        >Edit Post</RouterLink
+      >
       <h1>{{ post.title }}</h1>
       <div v-html="post.html" />
     </div>
