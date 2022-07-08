c0e9856d0880b220c7abfcd3195987c9f889812e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-08 10:39:55 +1000

    61 - race condition

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,14 @@
 import { computed } from 'vue';
 import Navbar from './components/Navbar.vue';
 import { useModal } from './composables/modal'
-import { useUsers } from './stores/users';
 
 const modal = useModal()
-const usersStore = useUsers()
 
 const modalStyle = computed(() => {
   return {
     display: modal.show.value ? 'block' : 'none'
   }
 })
-
-usersStore.authenticate()
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,20 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia"
 import { router } from "./router"
 import App from './App.vue'
+import { useUsers } from './stores/users';
+import { usePosts } from './stores/posts';
 
-createApp(App)
-.use(createPinia())
-.use(router)
-.mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+
+const usersStore = useUsers();
+const postsStore = usePosts();
+
+Promise.all([
+  usersStore.authenticate(),
+  postsStore.fetchPosts()
+]).then(() => {
+  app.use(router)
+  app.mount('#app')
+})
+
